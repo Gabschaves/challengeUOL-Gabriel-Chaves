4d455e78e1f57f46414b466d9659fb8de618f836
Gabriel Chaves <gabriel11schaves@gmail.com>
2023-09-12 18:30:02 -0300

    dependency update

---
+++ src/test/java/com/CompassUOL/ChallengeUOL/ChallengeUolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compassUOL.ChallengeUOL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeUolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
