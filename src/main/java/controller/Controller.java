cb5bb50c66f5589381b25640c10468175cff6941
Gabriel Chaves <gabriel11schaves@gmail.com>
2023-09-14 15:36:36 -0300

    class-controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,6 +3,8 @@ package controller;
 import model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import repository.RepositoryCar;
 
@@ -15,5 +17,9 @@ public class Controller {
     @GetMapping("/car")
     public List<Car> carList(){
         return repositoryCar.findAll();
+    }
+    @PostMapping("/car")
+    public Car add(@RequestBody Car car){
+        return repositoryCar.save(car);
     }
 }
